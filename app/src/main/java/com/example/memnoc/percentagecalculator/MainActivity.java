d4c130e14552f2b6da860b0f9c93ace85b0e314d
Memnoc <smartdroidesign@gmail.com>
2017-10-30 21:31:09 +0000

    Update MainActivity to run the math for the percentage.

--- app/src/main/java/com/example/memnoc/percentagecalculator/MainActivity.java
+++ app/src/main/java/com/example/memnoc/percentagecalculator/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.memnoc.percentagecalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView totalTextView;
+    EditText percentageTxt;
+    EditText numberTxt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        totalTextView = (TextView) findViewById(R.id.totalTextView);
+        percentageTxt = (EditText) findViewById(R.id.percentageField);
+        numberTxt = (EditText)  findViewById(R.id.numberField);
+
+
+        Button calcBtn = (Button) findViewById(R.id.calcButton);
+        calcBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                float percentage = Float.parseFloat(percentageTxt.getText().toString());
+                float dec = percentage / 100;
+                float total = dec * Float.parseFloat(numberTxt.getText().toString());
+                totalTextView.setText(Float.toString(total));
+
+            }
+        });
     }
+
 }
+
